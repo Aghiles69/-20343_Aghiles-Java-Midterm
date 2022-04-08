5f88bf46acf33f5d184a0218d28e0de9f8679ba3
Aghiles <Aghilesham20@gmail.com>
2022-04-07 22:28:57 -0500

    Solved string_problems.Anagram

--- src/string_problems/Anagram.java
+++ src/string_problems/Anagram.java
@@ -1,5 +1,7 @@
 package string_problems;
 
+import java.util.Arrays;
+
 public class Anagram {
 
     /*
@@ -14,5 +16,27 @@ public class Anagram {
     */
 
     //Implement Here
-
+    public static void main(String[] args) {
+        isAnagram("Bonnie","Clyde");
+        isAnagram("Ivan Bliminse","Invisible man");
+    }
+    static void isAnagram(String str1, String str2) {
+        String s1 = str1.replaceAll("", "");
+        String s2 = str2.replaceAll("", "");
+        boolean status = true;
+        if (s1.length() != s2.length()) {
+            status = false;
+        } else {
+            char[] ArrayS1 = s1.toLowerCase().toCharArray();
+            char[] ArrayS2 = s2.toLowerCase().toCharArray();
+            Arrays.sort(ArrayS1);
+            Arrays.sort(ArrayS2);
+            status = Arrays.equals(ArrayS1, ArrayS2);
+        }
+        if (status) {
+            System.out.println(s1 + " and " + s2 + " are anagrams");
+        } else {
+            System.out.println(s1 + " and " + s2 + " are not anagrams");
+        }
+    }
 }
